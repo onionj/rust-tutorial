34049fcd8a72c391e7cb45e41e36b14600ba9fbc
onionj <onionj98@gmail.com>
2021-10-06 16:12:42 +0330

    some simple code..

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,12 @@
 fn main(){
     println!("hello world");
+
+    let mut num = 10;
+    println!("format {} arguments", num);
+    num = 11;
+    println!("format {} arguments", num);
+
+    
+    const X:i32 = 12;
+    println!("format {} arguments", X);
 }
